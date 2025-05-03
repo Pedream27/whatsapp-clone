745ccf90f2c755c5372fa8494dd44588b16b76ab
Pedro Saraiva <pedreamsaraiva@gmail.com>
2025-05-03 15:43:04 -0300

    Feat: Commit inicial mais configuração de segurança

---
+++ Whatsapp-clone-api/src/main/java/br/com/phsaraiva/Whatsapp_clone_api/WhatsappCloneApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.phsaraiva.Whatsapp_clone_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappCloneApiApplication.class, args);
+	}
+
+}
---
+++ Whatsapp-clone-api/src/test/java/br/com/phsaraiva/Whatsapp_clone_api/WhatsappCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phsaraiva.Whatsapp_clone_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
